17b7bef59551c416a1342c3aeac11661afb3e900
veronika-a <38289933+veronika-a@users.noreply.github.com>
2021-10-19 09:44:36 +0200

    add UI Components

---
+++ 8BallDelegator/UI Components/CornerRadiusButton.swift
@@ -0,0 +1,36 @@
+//
+//  CornerRadiusButton.swift
+//  8BallDelegator
+//
+//  Created by Veronika Andrianova on 18.10.2021.
+//
+
+import UIKit
+
+@IBDesignable
+class CornerRadiusButton: UIButton {
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
---
+++ 8BallDelegator/UI Components/CornerRadiusView.swift
@@ -0,0 +1,37 @@
+//
+//  CornerRadiusView.swift
+//  8BallDelegator
+//
+//  Created by Veronika Andrianova on 18.10.2021.
+//
+
+import UIKit
+
+@IBDesignable
+class CornerRadiusView: UIView {
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
+
